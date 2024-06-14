bb237eff2ec8b32df2ce9d0b9cf5e68246f9adbb
Quin Derbi Kusuma <quinderbi@gmail.com>
2024-06-15 04:38:37 +0700

    Update getEndPoint.py

--- getEndPoint.py
+++ getEndPoint.py
@@ -8,6 +8,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 
+nltk.download('stopwords')
+nltk.download('punkt')
+
 stop_words = set(stopwords.words('english'))
 
 def preprocess_data(message):
